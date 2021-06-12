aa144ef14aabc7438e8c7e4481d088b7a0269e03
Cade Gordon <cadergator1@hotmail.com>
2021-06-11 19:08:36 -0500

    🥞 Fix batch collection

--- data/text_image_dm.py
+++ data/text_image_dm.py
@@ -146,9 +146,9 @@ class TextImageDataModule(LightningDataModule):
         self.dataset = TextImageDataset(self.folder, image_size=self.image_size, resize_ratio=self.resize_ratio, shuffle=self.shuffle, custom_tokenizer=not self.custom_tokenizer is None)
     
     def train_dataloader(self):
-        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=self.num_workers, drop_last=True)
+        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=self.num_workers, drop_last=True, collate_fn=self.dl_collate_fn)
     
-    def on_before_batch_transfer(self, batch, dataloader_idx):
+    def dl_collate_fn(self, batch):
         if self.custom_tokenizer is None:
             return batch
         else:
